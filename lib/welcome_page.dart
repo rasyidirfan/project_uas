ed9525914c9134ac8364f95f013c3c11d63436cf
irfanrasyid <irfanrasyid06@gmail.com>
2022-07-07 19:22:31 +0700

    tugas uas terbaru

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -14,57 +14,58 @@ class WelcomePage extends StatelessWidget {
       body: Container(
         color: Colors.white,
         child:
-          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Image.asset('assets/images/bg-login.png'),
-            Image.asset(
-              'assets/images/logo.png',
-              width: 194,
-              height: 67,
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Container(
+            margin: EdgeInsets.only(top: 250),
+            child: Image.network(
+              'https://www.creatopy.com/blog/wp-content/uploads/2020/05/apple-think-different-slogan.png',
             ),
-            Container(
-              padding: const EdgeInsets.only(left: 22, right: 22),
-              margin: EdgeInsets.only(top: 29),
-              height: 56,
-              child: ElevatedButton(
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 22, right: 22),
+            margin: EdgeInsets.only(top: 29),
+            height: 56,
+            child: ElevatedButton(
                 onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: ((context) {
-                      return Product();
-                    })));
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: ((context) {
+                    return Product();
+                  })));
                 },
                 style: ElevatedButton.styleFrom(
-                  primary: Color.fromRGBO(30, 35, 44, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12))),
-                child: Text('Login',
-                style: loginText,)),
-            ),
-            
-            Container(
+                    primary: Color.fromRGBO(30, 35, 44, 1),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                child: Text(
+                  'Login',
+                  style: loginText,
+                )),
+          ),
+          Container(
               padding: EdgeInsets.only(right: 22, left: 22),
               margin: EdgeInsets.only(top: 15, bottom: 29),
               height: 56,
               child: TextButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context, 
-                    MaterialPageRoute(builder: ((context) {
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: ((context) {
                       return RegisterPage();
                     })));
-                },
-                style: TextButton.styleFrom(
-                  side: BorderSide(
-                    color: Color.fromRGBO(30, 35, 44, 1),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12))),
-                child: Text('Register',
-                style: RegisterText))),
-            Center(child: Text('Continue as a Guest',
-            style: guestText,))
-          ]),
-        ),
+                  },
+                  style: TextButton.styleFrom(
+                      side: BorderSide(
+                        color: Color.fromRGBO(30, 35, 44, 1),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  child: Text('Register', style: RegisterText))),
+          Center(
+              child: Text(
+            'Continue as a Guest',
+            style: guestText,
+          ))
+        ]),
+      ),
     );
   }
 }
